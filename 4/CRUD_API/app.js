6da4ef2a2f863bd9f8760269ce6e0d9f3549e3c4
william <williamjean.k.o@gmail.com>
2021-02-28 12:53:29 +0100

    Made CRUD_API with different ways to complete the task for each part of CRUD

---
+++ 4/CRUD_API/app.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express() 
+
+app.use(express.json());
+const dataArray = new Array();
+
+
+// GET 
+app.get('/', function (req, res) {
+    console.log(dataArray);
+  res.send("<h1>Front Page</h1>");
+})
+
+// POST
+app.post("/data", (req, res) => {
+    console.log(req.body);
+    dataArray.push(req.body);
+    console.log(dataArray);
+    res.send({ body: req.body });
+});
+
+// PUT
+app.put("/data/:value", (req, res) => {
+    let value = req.params.value;
+    let newValue = req.body;
+    console.log(req.body);
+    if(dataArray.indexOf(value)){
+        dataArray.splice(value,1,newValue)
+    }
+    console.log(dataArray);
+    res.send({ body: req.body });
+});
+
+// DELETE by using the url with "/?value=(name)" fx "/?value=Sen" deletes Sen from array
+app.delete("/data", (req,res) => {
+    console.log(req.body);
+    dataArray.splice(req.body,1);   
+    res.send({ body: req.body });
+    console.log(dataArray);
+});
+
+app.listen(8080)
