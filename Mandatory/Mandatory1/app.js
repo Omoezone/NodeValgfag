91445e1b0e867ecef031d1718be8bd07fd1aa9b8
william <williamjean.k.o@gmail.com>
2021-03-13 14:40:43 +0100

    Basicly done with the project, might need some more work on the css

--- Mandatory/Mandatory1/app.js
+++ Mandatory/Mandatory1/app.js
@@ -9,17 +9,20 @@ app.get("/basic", (req, res) =>{
     res.sendFile(__dirname + '/views/basic.html');
 });
 
-app.get("/expressTerminal", (req, res) =>{
-    res.sendFile(__dirname + '/views/expressTer.html');
+app.get("/express", (req, res) =>{
+    res.sendFile(__dirname + '/views/express.html');
 });
 
-app.get("/focusedTopic", (req, res) =>{
-    res.sendFile(__dirname + '/views/focusedTop.html');
+app.get("/terminal", (req, res) =>{
+    res.sendFile(__dirname + '/views/terminal.html');
 });
+
+const port = process.env.PORT || 8080;
+
 app.listen(8080, (error) => {
     if(error){
         console.log("The program experienced an error" + error);
     }else {
-        console.log("Starting program on port: ", 8080);
+        console.log("Starting program on port: ", port);
     }
 });
